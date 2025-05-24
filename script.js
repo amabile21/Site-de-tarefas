d239a2a3ac52d997968c561c98f272ad5ef545d9
amabile21 <amabiletalia@gmail.com>
2025-05-24 01:39:42 -0300

    JS

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
-document.getElementById('add').addEventListener('click', function() {
-    const input = document.getElementById('digite');
-    const valorInput = input.value.trim();
+document.getElementById("add").addEventListener("click", function () {
+  const input = document.getElementById("digite");
+  const valorInput = input.value.trim();
 
-    if (valorInput) {
-        const itemLista = document.createElement("li");
-        itemLista.className = "item-tarefa";
-        const spanTexto = document.createElement("span");
-        spanTexto.textContent = valorInput;
+  if (valorInput) {
+    const itemLista = document.createElement("li");
+    itemLista.className = "item-tarefa";
+    const spanTexto = document.createElement("span");
+    spanTexto.textContent = valorInput;
 
-        const checkbox = document.createElement("input");
-        checkbox.type = "checkbox";
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
 
-        //popup
-        checkbox.addEventListener('change', function () {
-            if (checkbox.checked) {
-                alert("Parabéns!");
-            }
-        });
+    //popup
+    checkbox.addEventListener("change", function () {
+      if (checkbox.checked) {
+        alert("Parabéns!");
+      }
+    });
 
-        itemLista.appendChild(spanTexto);
-        itemLista.appendChild(checkbox);
+    itemLista.appendChild(spanTexto);
+    itemLista.appendChild(checkbox);
 
-        document.getElementById("listaTarefas").appendChild(itemLista);
-        input.value = '';
-    }
+    document.getElementById("listaTarefas").appendChild(itemLista);
+    input.value = "";
+  }
 });
 
 //limpa a lista
-document.getElementById('Limpar').addEventListener('click', function() {
-    document.getElementById('listaTarefas').innerHTML = '';
+document.getElementById("Limpar").addEventListener("click", function () {
+  document.getElementById("listaTarefas").innerHTML = "";
 });
